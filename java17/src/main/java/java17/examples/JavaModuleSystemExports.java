e38f46661200f9ed3c4bb45b5207318fbb556619
HKBUSeanTYH <seantyh118@gmail.com>
2025-03-23 18:25:27 +0800

    adding details via comment

--- java17/src/main/java/java17/examples/JavaModuleSystemExports.java
+++ java17/src/main/java/java17/examples/JavaModuleSystemExports.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public class JavaModuleSystemExports {
     /*
-        Java 9 module system
+        Java 9 module system (Project Jigsaw)
+        - Opt-in and backwards-compatible module system to avoid ClassDefNotFoundErrors at runtime and create internal APIs
+
         exporting the base class from java8 module and requiring java8 by java17
      */
     public static void main(String[] args) {
